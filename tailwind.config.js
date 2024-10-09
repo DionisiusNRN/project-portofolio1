d23df3a5d2c588adce59ceccc15accd9e4ad8dac
dionisiusnusaca <dnur3no@gmail.com>
2024-10-09 17:32:36 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        experience: "#081e25",
+      },
+      height: {
+        350: "21.8rem",
+      },
+    },
+  },
+  plugins: [],
+};
